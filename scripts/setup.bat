9a82a789675ad64ba63a9da8d01fa3e0cafabfb3
AnderCMD <andercmd@outlook.com>
2026-04-20 13:48:11 -0600

    feat: add setup.bat script to automate virtual environment creation and dependency installation

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -6,10 +6,10 @@ echo ==============================================
 echo.
 
 if not exist "..\.venv" (
-    echo [1/3] Creating local virtual environment (.venv)...
+    echo [1/3] Creating local virtual environment .venv...
     python -m venv ..\.venv
 ) else (
-    echo [1/3] Virtual environment (.venv) already exists.
+    echo [1/3] Virtual environment .venv already exists.
 )
 
 echo [2/3] Activating environment and installing dependencies (requirements.txt)...
